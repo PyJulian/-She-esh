aaab16eb9597dd187a16c2fa590ad9f5976e41c5
Sophie <worteljulian2@gmail.com>
2026-04-23 23:48:20 +0200

    OG + Twitter image toevoegen
    hopelijk werkt dit voor alles

--- db/meta.php
+++ db/meta.php
@@ -1,3 +1,11 @@
+<?php
+	$prot = isset($_SERVER['HTTPS']) ? 'https' : 'http'; // Get protocol
+
+	$addr = $prot . '://' . str_replace('httpdocs/', '',
+		str_replace('/var/www/vhosts/', '', __DIR__)
+	) . '/banner.png'; // Get banner image on ever type of server (Ignore all str_replaces thats to remove vhost path from my school server lol)
+?>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -5,5 +13,7 @@
 <meta name="author" content="NeCanna / Sophie">
 <meta name="keywords" content="NeCanna, Sophie, JS, PHP, ASM, x86, Python, Portofolio, CV">
 <meta name="robots" content="index, follow, notranslate">
+<meta property="og:image" content="<?= $addr ?>">
+<meta property="twitter:image" content="<?= $addr ?>">
 
 <link rel="stylesheet" type="text/css" href="style.css">
